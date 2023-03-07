304706139c25b1424e5936b5a4f07b84022fa66f
goeunP <0young384@gmail.com>
2023-03-07 15:31:06 +0900

    set dashboard

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 // Buttons
 const startBtn = document.getElementById("btn-start");
-
+const dashboardBtn = document.getElementById("btn-dash");
 const startQuiz = () => {
-    window.location.href = "/quiz.html";
+  window.location.href = "/quiz.html";
+};
+const navigateToDashboard = () => {
+  window.location.href = "/dashboard.html";
 };
 
 startBtn.addEventListener("click", startQuiz);
+dashboardBtn.addEventListener("click", navigateToDashboard);
